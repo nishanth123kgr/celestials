80dacdf617f748957589e4d22f0a5646de2f41f9
Nishanth R <nishanth123kgr@gmail.com>
2023-10-08 02:52:54 +0530

    added audio

--- src/js/model.js
+++ src/js/model.js
@@ -1,12 +1,32 @@
 const models = ['psep.glb', 'isotope_heater.gltf', 'seismometer.glb', 'antenna.gltf', 'solar.gltf']
 
+const audios = ['psep', 'isotope_heater', 'seismometer', 'antenna', 'solar']
+
+let audio_pref = 'en', audio = null;
+let modelIndex = 0;
+
+document.getElementById('audio-pref').addEventListener('change', (event) => {
+  audio_pref = event.target.value;
+});
+
+document.getElementById('play_button').addEventListener('click', (event) => {
+  if(!audio || audio.paused){
+    event.target.innerHTML = 'Pause Audio';
+    audio = document.createElement('audio');
+    audio.src = `../src/audio/${audios[modelIndex]}_${audio_pref}.mp3`;
+    audio.play();
+  } else {
+    event.target.innerHTML = 'Play Audio';
+    audio.pause();
+  }
+});
 
 
 
 function changeModel(elem) {
   document.querySelector(".active").classList.remove("active");
   elem.classList.add("active");
-  let modelIndex = elem.getAttribute('data-i');
+  modelIndex = elem.getAttribute('data-i');
   document.querySelector('model-viewer').setAttribute('src', `../src/models/${models[modelIndex]}`);
   if(modelIndex !== 0){
     document.querySelectorAll('button').forEach((button) => {
