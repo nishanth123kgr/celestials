703e6779a5184c9dacd7a76410f218ff4e657ee6
Nishanth R <nishanth123kgr@gmail.com>
2023-10-08 03:19:30 +0530

    completed ar page

--- src/js/model.js
+++ src/js/model.js
@@ -1,6 +1,16 @@
-const models = ['psep.glb', 'isotope_heater.gltf', 'seismometer.glb', 'antenna.gltf', 'solar.gltf']
+const models = ['psep.glb', 'isotope_heater.gltf', 'seismometer.glb', 'antenna.gltf', 'solar.gltf', 'lander.gltf']
 
 const audios = ['psep', 'isotope_heater', 'seismometer', 'antenna', 'solar']
+
+const descriptions = [
+  ['PSEP', `The Passive Seismic Experiment was the first seismometer placed on the Moon's surface. It studied the propagation of seismic waves through the Moon and provided the first detailed look at the Moon's internal structure. This instrument featured four seismometers powered by two solar panels deployed on either side and a dedicated antenna pointed towards our planet.
+<br><br>Regrettably, the seismic instrument package ceased responding to commands at 0400 UT on August 25, 1969. This was likely due to overheating caused by the scorching midday lunar sun. 
+`], 
+  ['Isotope Heater', `The first major use of nuclear energy in a NASA manned mission happened when a radioisotope heater was used to keep the instrument to a minimum of minus 54ºC as the temperatures can plummet to minus 170ºC during the 340 hour lunar night .`] ,
+  ['Seismometer',  `This instrument featured four seismometers, strategically positioned to measure both meteorite impacts and moonquakes. Over its operational lifetime, it recorded an impressive tally of approximately 100 to 200 meteorite impacts.`],
+  ['Antenna', `Crucial seismic event data, including strength, duration, and approximate direction, were relayed from the lunar surface to tracking stations on Earth via a dedicated antenna pointed towards our planet.`],
+  ['Solar Panel', `The Passive Seismic Experiment (PSE) relied solely on solar cells situated on either side of the instrument, allowing the experiment to function exclusively during lunar daylight periods.`]
+]
 
 let audio_pref = 'en', audio = null;
 let modelIndex = 0;
@@ -10,7 +20,7 @@ document.getElementById('audio-pref').addEventListener('change', (event) => {
 });
 
 document.getElementById('play_button').addEventListener('click', (event) => {
-  if(!audio || audio.paused){
+  if (!audio || audio.paused) {
     event.target.innerHTML = 'Pause Audio';
     audio = document.createElement('audio');
     audio.src = `../src/audio/${audios[modelIndex]}_${audio_pref}.mp3`;
@@ -27,16 +37,22 @@ function changeModel(elem) {
   document.querySelector(".active").classList.remove("active");
   elem.classList.add("active");
   modelIndex = elem.getAttribute('data-i');
+  console.log(typeof (modelIndex));
   document.querySelector('model-viewer').setAttribute('src', `../src/models/${models[modelIndex]}`);
-  if(modelIndex !== 0){
+  if (modelIndex != 0) {
     document.querySelectorAll('button').forEach((button) => {
-      button.style.display = 'none';
+      console.log('button removed');
+      if (button.id !== 'play_button')
+        button.style.display = 'none';
     });
   } else {
     document.querySelectorAll('button').forEach((button) => {
+      console.log('button added');
       button.style.display = 'block';
     });
   }
+  document.getElementById('desc_title').innerHTML = descriptions[modelIndex][0];
+  document.getElementById('desc_body').innerHTML = descriptions[modelIndex][1];
 }
 
 
